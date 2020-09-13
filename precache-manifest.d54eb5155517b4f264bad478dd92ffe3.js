9f5e04ebe979ea221be5d697616410ec4e06fed6
Jasmine Humbert <jasmine@humbert.dev>
2020-09-13 00:32:49 -0700

    Updates

---
+++ precache-manifest.d54eb5155517b4f264bad478dd92ffe3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d13987b3a896fa113fdc6d29ef98605",
+    "url": "/AQI2cigarettes/index.html"
+  },
+  {
+    "revision": "a7e0a0eadf2257e5480a",
+    "url": "/AQI2cigarettes/static/css/2.fcdba597.chunk.css"
+  },
+  {
+    "revision": "2cb0c45cd9e536738453",
+    "url": "/AQI2cigarettes/static/css/main.e261cc6f.chunk.css"
+  },
+  {
+    "revision": "a7e0a0eadf2257e5480a",
+    "url": "/AQI2cigarettes/static/js/2.1ad8f0ec.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/AQI2cigarettes/static/js/2.1ad8f0ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cb0c45cd9e536738453",
+    "url": "/AQI2cigarettes/static/js/main.1c7c2a26.chunk.js"
+  },
+  {
+    "revision": "87664d4a4893a17b09bb",
+    "url": "/AQI2cigarettes/static/js/runtime-main.aa3adcc7.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/AQI2cigarettes/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/AQI2cigarettes/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/AQI2cigarettes/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/AQI2cigarettes/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/AQI2cigarettes/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/AQI2cigarettes/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/AQI2cigarettes/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/AQI2cigarettes/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/AQI2cigarettes/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/AQI2cigarettes/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/AQI2cigarettes/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/AQI2cigarettes/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/AQI2cigarettes/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/AQI2cigarettes/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/AQI2cigarettes/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/AQI2cigarettes/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
