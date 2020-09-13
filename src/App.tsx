b0d19f53d613497bad9db90949f2953fe6342315
Jasmine Humbert <jasmine@humbert.dev>
2020-09-12 23:49:08 -0700

    [add] react stuff jank but functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import 'semantic-ui-react'
+import { Container, Header, Input, Button } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component<
+  {},
+  { AQI: number; hours: number; cigs: any }
+> {
+
+  constructor (props: any) {
+    super(props)
+
+    this.state = {
+      AQI: 150,
+      hours: 24,
+      cigs: "stuff"
+    }
+  }
+
+  AQI2cigs () {
+    // placeholder while I get react shit working
+    var { AQI, hours } = this.state
+    const cigs = AQI + hours
+    this.setState({ cigs: cigs })
+    this.forceUpdate()
+  }
+
+  render () {
+    const { AQI, hours, cigs } = this.state
+    return (
+      <div className='App'>
+        <Header as='h1'>AQI to Cigarettes Calculator</Header>
+        <br />
+        <br />
+        <Container className='max-200'>
+          <Input fluid 
+            label='AQI' 
+            value={AQI}
+            onChange={e => this.setState({AQI: Number(e.currentTarget.value) })} 
+          />
+          <br />
+          <br />
+          <Input fluid 
+            label='Hours Exposed' 
+            value={hours}
+            onChange={e => this.setState({hours: Number(e.currentTarget.value) })} 
+            />
+          <br />
+          <br />
+          <Button primary onClick={this.AQI2cigs.bind(this)}>
+            Calculate
+          </Button>
+          <br />
+          <br />
+          <p>It's like you've smoked</p>
+          <Header as='h2'>{cigs}</Header>
+          <p>cigarettes</p>
+        </Container>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'semantic-ui-css/semantic.min.css'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
