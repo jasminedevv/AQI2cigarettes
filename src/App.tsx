8135d3c4ab653a71b8c8776c562065f0b4f24cfe
Jasmine Humbert <jasmine@humbert.dev>
2020-09-13 17:53:51 -0700

    [add] AQI to cigarettes kinda works but is ~50% off from the examples

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ class App extends React.Component<
 
     // Source: http://berkeleyearth.org/archive/air-pollution-and-cigarette-equivalence/
     const particlesPerCigPerHours = 22
-    const cigs = (PM24h / particlesPerCigPerHours) / 24 * hours
+    const cigs = (PM24h / particlesPerCigPerHours / 24) * hours
 
     this.setState({ cigs })
   }
@@ -106,31 +106,31 @@ const AQITablePM25 = {
   concentrations: [
     {
       min: 0.0,
-      max: 12.0,
+      max: 15.5,
       index: {
         min: 0,
         max: 50
       }
     },
     {
-      min: 12.1,
-      max: 35.4,
+      min: 15.5,
+      max: 40.5,
       index: {
         min: 51,
         max: 100
       }
     },
     {
-      min: 35.5,
-      max: 55.4,
+      min: 40.5,
+      max: 65.5,
       index: {
         min: 101,
         max: 150
       }
     },
     {
-      min: 55.5,
-      max: 150.4,
+      min: 65.5,
+      max: 150.5,
       index: {
         min: 151,
         max: 200
@@ -138,7 +138,7 @@ const AQITablePM25 = {
     },
     {
       min: 150.5,
-      max: 250.4,
+      max: 250.5,
       index: {
         min: 201,
         max: 300
@@ -146,56 +146,26 @@ const AQITablePM25 = {
     },
     {
       min: 250.5,
-      max: 350.4,
+      max: 500.5,
       index: {
         min: 301,
         max: 400
       }
     },
     {
-      min: 350.5,
-      max: 500.4,
+      min: 500.5,
+      max: 99999999999,
       index: {
         min: 401,
-        max: 500
+        max: 99999999999
       }
     }
   ]
 }
 
-function getBreakpoints (AQI: number) {
-  const breakpoints = AQITablePM25.concentrations.find(function (conc) {
-    if (conc.index.min < AQI && conc.index.max > AQI) {
-      return conc
-    }
-  })
-  if (breakpoints === undefined) {
-    throw new Error('concentration out of bounds')
-  }
-  return breakpoints
-}
-
-// calculates PM 2.5 concentration per 24 hours based on AQI
-function calcPM25 (AQI: number) {
-  const breakpoints = getBreakpoints(AQI)
-  const PM_min = breakpoints.min
-  const PM_max = breakpoints.max
-  const AQI_min = breakpoints.index.min
-  const AQI_max = breakpoints.index.max
-
-  const PM =
-    (PM_min * AQI_max - PM_min * AQI - AQI_min * PM_max + PM_max * AQI) /
-    (PM_max - AQI_min)
-
-  console.log(
-    `For AQI of ${AQI}, found particle concentration of ${PM} for 24 hours`
-  )
-  return PM
-}
-
 function getBreakpointsParticles (particles: number) {
   const breakpoints = AQITablePM25.concentrations.find(function (conc) {
-    if (conc.index.min < particles && conc.index.max > particles) {
+    if (conc.index.min <= particles && conc.index.max > particles) {
       return conc
     }
   })
@@ -205,7 +175,6 @@ function getBreakpointsParticles (particles: number) {
   return breakpoints
 }
 
-/** Nothing below this line is working */
 function calcAQI (inputConcentration: number) {
   const breakpoints = getBreakpointsParticles(inputConcentration) // should be
   const PM_observed = inputConcentration
@@ -219,4 +188,40 @@ function calcAQI (inputConcentration: number) {
     ((PM_observed - PM_min) * (AQI_max - AQI_min)) / (PM_max - PM_min) + AQI_min
 
   return AQI
+}
+
+function getBreakpoints (AQI: number) {
+  const breakpoints = AQITablePM25.concentrations.find(function (conc) {
+    if (conc.index.min <= AQI && conc.index.max > AQI) {
+      return conc
+    }
+  })
+  if (breakpoints === undefined) {
+    throw new Error('AQI out of bounds')
+  }
+  return breakpoints
+}
+
+// calculates PM 2.5 concentration per 24 hours based on AQI
+// None of this works currently
+function calcPM25 (AQI: number) {
+  const breakpoints = getBreakpoints(AQI)
+  const PM_min = breakpoints.min
+  const PM_max = breakpoints.max
+  const AQI_min = breakpoints.index.min
+  const AQI_max = breakpoints.index.max
+
+  console.log(breakpoints)
+
+  // const PM =
+  //   (PM_min * AQI_max - PM_min * AQI - AQI_min * PM_max + PM_max * AQI) /
+  //   (PM_max - AQI_min)
+
+  const PM =
+    ((PM_max - PM_min) * (AQI - AQI_min)) / (AQI_max - AQI_min) + PM_min
+
+  console.log(
+    `For AQI of ${AQI}, found particle concentration of ${PM} for 24 hours`
+  )
+  return PM
 }
